3079a8ef81b0429d31408200d9e5a1b065896294
diishqa <sfsfgrfhfjhdhdfghdfgh>
2025-09-27 11:18:16 +0300

    c

---
+++ src/add.kt
@@ -0,0 +1,146 @@
+import kotlin.math.atan2
+
+fun main() {
+    //val namber = readln().toInt()
+    //println(namber)
+
+    /*print("Введите первое число: ")
+    val n1 = readln().toInt()
+    print("Введите второе число: ")
+    val n2 = readln().toInt()
+    if (n1 > n2){
+        println("первое число больше ")
+    }
+    else if (n1 == n2) {
+        println("равны")
+    }
+    else {
+        println("второе число больше ")
+    }
+    val max = if (n1 > n2) n1 else n2
+    println(max)*/
+    /*val age = readln().toInt()
+    val st = if (age >= 18) "Взрослый" else "ребенок"
+    println(st)*/
+
+    /*val n = 1 until 100
+    val n1 = 100 downTo 1 step 5
+    val s = "a".."z"
+    for(i in n) {
+        println(i)
+    }*/
+
+    /*print("Введите порядковый номер месяца 1-12 ")
+    val n = readln().toInt()
+    var month = when (n) {
+        1 ->  "Январь"
+        2 ->  "Февраль"
+        3 ->  "Март"
+        4 ->  "Апрель"
+        5 -> "май"
+        6 -> "Июнь"
+        7 ->  "Июль"
+        8 ->  "Август"
+        9 ->  "Сентябрь"
+        10 -> "Октябрь"
+        11 -> "Ноябрь"
+        12 ->  "Декабрь"
+        else -> "нет"
+    }
+    if ( n in 1..12){
+        println("Ваш индекс $n оотвествует месяцу $month")
+    }*/
+
+    //    print("Введите урон: ");
+//    val uron = readln().toInt()
+//    print("Щит активен? (введите да или нет):");
+//    val ot : String = readln()
+//    if (ot == "да"){
+//        println(uron/2)
+//    }
+//    else {
+//        println(uron)
+//    }
+
+    /*println("Введите level: ")
+    val n = readln().toInt()
+    println("Есть ли у вас вип статус: ")
+    val a = readln().toBoolean()
+    if (n >= 30 && a || n >= 50){
+        println("Доступ разрешен!")}
+    else {
+        println("Доступ запрещен!")
+    }*/
+
+    /*println("Введите level: ")
+    val n = readln().toInt()
+    if (n in 1..50) {
+        println("Мы вошли в дандж")
+    }
+    else {
+        println("Уходи")
+    }*/
+
+
+    /*val b = "z" .. "a"
+
+    val s = 5 until 11
+    for(i in s) {
+        println(i)
+    }
+    println("-----------------------------------------------------------")
+    val a = 10 until 50 step 3
+    for(i in a) {
+        println(i)
+    }*/
+
+    fun main() {
+        /*repeat(times = 5){
+            println("Начинаем ритуал защиты!")
+            println("Заклинание защиты активировано!")
+            println("Заклинание защиты активировано!")
+            println("Заклинание защиты активировано!")
+            println("Заклинание защиты активировано!")
+            println("Заклинание защиты активировано!")
+            println("Замок защищен!")
+        var index = 0
+            repeat(times = 5) {
+                println("Заклинание защиты активировано! (Шаг ${index + 1})")
+                index ++
+            }
+        }*/
+
+    }
+    //Домашнее задание
+    //1
+//    print("Введите свой уровень: ")
+//    val level  = readln().toInt()
+//    if (level < 10){
+//        println("Новичок! получаете бонус +10 HP")
+//    }
+//    else if  (10 <= level && level < 50 ){
+//        println("Опытный! получаете +30 HP")
+//    }
+//    else{
+//        println("Легенда! получаете +100 HP")
+//    }
+
+    //2
+//    print("Введите свой возраст: ")
+//    val age = readln().toInt()
+//    if (age < 12){
+//        println("Вы ребёнок")
+//    }
+//    else if (age >= 12 && age <= 17) {
+//        println("Вы подросток")
+//    }
+//    else {
+//        println("Вы взрослый")
+//    }
+
+    //3
+
+
+
+
+}
